67417d46b63727a6c60a5c5461551666608d7225
Andy-git985 <andy.yu617@gmail.com>
2023-06-29 22:21:29 -0400

    change api base url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const baseQuery = fetchBaseQuery({
   baseUrl:
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3001'
-      : 'https://cutaboveshop-api.onrender.com',
+      ? 'http://localhost:8080'
+      : 'https://cutaboveshop-api.fly.dev/',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     return headers;
