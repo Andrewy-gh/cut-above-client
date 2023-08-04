bfb72d53f9da012a53d1a1a0b37e704098ba7446
Andy-git985 <andy.yu617@gmail.com>
2023-08-03 21:21:42 -0400

    Footer and appointment sorting

--- src/components/navbar/NavDesktop.jsx
+++ src/components/navbar/NavDesktop.jsx
@@ -1,42 +1,39 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import IconButton from "@mui/material/IconButton";
-import { theme } from "../../styles/styles";
-import { selectCurrentToken } from "../../features/auth/authSlice";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+import { theme } from '../../styles/styles';
+import { selectCurrentToken } from '../../features/auth/authSlice';
 
 export default function NavDesktop() {
   const token = useSelector(selectCurrentToken);
 
   return (
-    <div
+    <ul
       style={{
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr",
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
       }}
     >
-      <div style={{ placeSelf: "center start" }}>
+      <li style={{ placeSelf: 'center start' }}>
         <Link to="/bookings">
           <Button variant="contained">Book now</Button>
         </Link>
-      </div>
-      <div style={{ placeSelf: "center center" }}>
-        <Link to="/">
-          <div
-            style={{
-              fontFamily: "Corben",
-              fontSize: "1.3125rem",
-              fontWeight: 700,
-            }}
-          >
-            Cut Above
-          </div>
-        </Link>
-      </div>
+      </li>
+      <li
+        style={{
+          placeSelf: 'center center',
+          fontFamily: 'Corben',
+          fontSize: '1.3125rem',
+          fontWeight: 700,
+        }}
+      >
+        <Link to="/">Cut Above</Link>
+      </li>
       {token ? (
-        <div style={{ placeSelf: "center end" }}>
+        <li style={{ placeSelf: 'center end' }}>
           <Link to="/account">
             <IconButton
               edge="start"
@@ -45,17 +42,17 @@ export default function NavDesktop() {
               <AccountCircleIcon fontSize="large" />
             </IconButton>
           </Link>
-        </div>
+        </li>
       ) : (
-        <div style={{ placeSelf: "center end" }}>
+        <li style={{ placeSelf: 'center end' }}>
           <Link to="/signup">
             <Button sx={{ color: theme.palette.secondary.dark }}>Signup</Button>
           </Link>
           <Link to="/login">
             <Button variant="contained">Login</Button>
           </Link>
-        </div>
+        </li>
       )}
-    </div>
+    </ul>
   );
 }
