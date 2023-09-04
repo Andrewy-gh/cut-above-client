dbff5cd02408e1cb01bd90edb1ad11c15b0b4adb
andy <andy.yu617@gmail.com>
2023-09-04 02:53:36 +0000

    reset pw

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { useAuth } from "../hooks/useAuth";
 import Overlay from "../components/Overlay";
 
 export default function Login() {
+  const [view, setView] = useState('login')
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
@@ -19,11 +20,17 @@ export default function Login() {
       setError(error);
     }
   };
-
-  return (
-    <Overlay>
-      <div style={{ width: "min(40ch, 100% - 2rem)", marginInline: "auto" }}>
-        <h3 style={{ textAlign: "center" }}>Log in</h3>
+  const handleReset = async (e) => {
+    e.preventDefault()
+    try {
+console.log('resetting pw')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  let content
+  if (view === 'login') {
+    content = (<><h3 style={{ textAlign: "center" }}>Log in</h3>
         <form onSubmit={handleSubmit}>
           <TextField
             label="Email"
@@ -52,6 +59,31 @@ export default function Login() {
             Login
           </Button>
         </form>
+        <button onClick={()=> setView('reset')}>Reset Password</button></>)
+  } else {
+    content = (<> <h3 style={{ textAlign: "center" }}>Reset Password</h3><form onSubmit={handleReset}>
+          <TextField
+            label="Email"
+            required
+            fullWidth
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          ></TextField>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 2, mb: 3 }}
+          >
+            Reset password
+          </Button>
+        </form><button onClick={()=> setView('login')}>Login</button></>)
+  }
+
+  return (
+    <Overlay>
+      <div style={{ width: "min(40ch, 100% - 2rem)", marginInline: "auto" }}>
+        {content}
       </div>
     </Overlay>
   );
