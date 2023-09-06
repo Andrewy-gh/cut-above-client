2e01f4289e3afb84f8cde6583f9a707488d88a26
Andy-git985 <andy.yu617@gmail.com>
2023-09-05 22:30:03 -0400

    remove try catch

--- src/components/account/ResetPw.jsx
+++ src/components/account/ResetPw.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import { useNotification } from '../../hooks/useNotification';
 import { useAccount } from '../../hooks/useAccount';
 
 export default function ResetPw() {
+  const navigate = useNavigate();
   let [searchParams, setSearchParams] = useSearchParams();
   const [isValidToken, setIsValidToken] = useState(false);
   const [newPassword, setNewPassword] = useState('');
@@ -18,21 +19,25 @@ export default function ResetPw() {
   const { handleError } = useNotification();
 
   useEffect(() => {
-    console.log('lol! the token is ', token);
-    fetch(`http://localhost:3001/api/user/validate-token/${token}`)
-      .then((response) => response.json())
-      .then((data) => {
+    async function validateResetToken(token) {
+      try {
+        const res = await fetch(
+          `http://localhost:3001/api/user/validate-token/${token}`
+        );
+        const data = res.json();
         if (data.message === 'Token is valid') {
           setIsValidToken(true);
         } else {
           handleError(
             'Invalid or expired token. Please request a new password reset link.'
           );
+          navigate('/login');
         }
-      })
-      .catch((err) => {
+      } catch (err) {
         handleError('An error occurred.');
-      });
+      }
+    }
+    validateResetToken(token);
   }, [token]);
 
   const handleNewPasswordChange = (e) => {
@@ -48,28 +53,25 @@ export default function ResetPw() {
   };
 
   const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      if (newPassword !== confirmNewPassword) {
-        setError(true);
-        setHelperText('Passwords do not match');
-        return;
-      }
-      const passwordChanged = await handleUserPasswordReset({
-        token,
-        newPassword,
-      });
-      if (passwordChanged) {
-        setNewPassword('');
-        setConfirmNewPassword('');
-      }
-    } catch (error) {
-      console.error(error);
+    e.preventDefault();
+    if (newPassword !== confirmNewPassword) {
+      setError(true);
+      setHelperText('Passwords do not match');
+      return;
+    }
+    const passwordChanged = await handleUserPasswordReset({
+      token,
+      newPassword,
+    });
+    if (passwordChanged) {
+      setNewPassword('');
+      setConfirmNewPassword('');
     }
   };
 
+  let content;
   if (isValidToken) {
-    return (
+    content = (
       <form onSubmit={handleSubmit}>
         <TextField
           label="New Password"
@@ -101,6 +103,23 @@ export default function ResetPw() {
         </Button>
       </form>
     );
+  } else {
+    content = (
+      <>
+        <h3 style={{ textAlign: 'center' }}>
+          Oops looks like an error happened...
+        </h3>
+        <Link to="/login">
+          <p style={{ textAlign: 'center' }}>
+            Click <u>here</u> to request a password reset again
+          </p>
+        </Link>
+      </>
+    );
   }
-  return <div>Incorrect token</div>;
+  return (
+    <div style={{ width: 'min(80ch, 100% - 2rem)', marginInline: 'auto' }}>
+      {content}
+    </div>
+  );
 }
