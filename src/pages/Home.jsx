10832b99be4ce56e2f114fa059817e9f75533200
Andy-git985 <andy.yu617@gmail.com>
2023-12-09 16:30:49 -0500

    team member css module

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import ContactUs from '@/components/home/ContactUs';
 import Services from '../components/home/Services';
-import TeamMembers from '../components/home/TeamMembers';
+import TeamMembers from '@/components/home/TeamMembers';
 import Hero from '../components/home/Hero';
 import { employees } from '../data/data';
 
