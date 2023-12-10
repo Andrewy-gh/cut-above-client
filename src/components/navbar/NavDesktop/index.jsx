ac3fab62764f91f86c12538b3f6bc0b655caf54b
Andy-git985 <andy.yu617@gmail.com>
2023-12-10 13:55:32 -0500

    NavDesktop css module

---
+++ src/components/navbar/NavDesktop/index.jsx
@@ -0,0 +1,49 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+import { theme } from '@/styles/styles';
+import { selectCurrentToken } from '@/features/auth/authSlice';
+import styles from './styles.module.css';
+
+export default function NavDesktop() {
+  const token = useSelector(selectCurrentToken);
+
+  return (
+    <ul className={styles.grid_col_3}>
+      <li style={{ placeSelf: 'center start' }}>
+        <Link to="/bookings">
+          <Button variant="contained">Book now</Button>
+        </Link>
+      </li>
+      <li
+        className={styles.list_item}
+        style={{ color: theme.palette.secondary.main }}
+      >
+        <Link to="/">Cut Above</Link>
+      </li>
+      {token ? (
+        <li className={styles.place_self}>
+          <Link to="/account">
+            <IconButton
+              edge="start"
+              sx={{ mr: 1, color: theme.palette.secondary.dark }}
+            >
+              <AccountCircleIcon fontSize="large" />
+            </IconButton>
+          </Link>
+        </li>
+      ) : (
+        <li className={styles.place_self}>
+          <Link to="/signup">
+            <Button sx={{ color: theme.palette.secondary.dark }}>Signup</Button>
+          </Link>
+          <Link to="/login">
+            <Button variant="contained">Login</Button>
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
+}
