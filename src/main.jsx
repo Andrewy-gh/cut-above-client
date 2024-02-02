948dea2f85563792eb7d106b1a33757cc2fbb57f
Andy-git985 <andy.yu617@gmail.com>
2024-02-01 21:09:51 -0500

    moved ResetPw to routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { store } from './app/store.js';
+import App from '@/App.jsx';
+import '@/index.css';
+import { store } from '@/app/store.js';
 import { Provider } from 'react-redux';
 
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { CssBaseline, ThemeProvider, responsiveFontSizes } from '@mui/material';
-import { theme } from './styles/styles.js';
+import { theme } from '@/styles/styles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
