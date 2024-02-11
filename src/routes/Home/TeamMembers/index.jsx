803d00ee35e3280ca340f4da9a37f3c39768e39b
Andy-git985 <andy.yu617@gmail.com>
2024-02-10 22:59:52 -0500

    moved fetching into teammembers and separated cards into separate file

--- src/routes/Home/TeamMembers/index.jsx
+++ src/routes/Home/TeamMembers/index.jsx
@@ -1,54 +1,32 @@
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Grid from '@mui/material/Grid';
+import { useGetEmployeesProfilesQuery } from '@/features/employeeSlice';
 import { useFilter } from '@/hooks/useFilter';
 import styles from './styles.module.css';
+import MemberCard from './MemberCard';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
-const MemberCard = ({ employee, handleClick }) => {
-  return (
-    <Grid item xs={12} sm={6} md={4} sx={{ marginInline: 'auto' }}>
-      <Card
-        sx={{
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <CardMedia
-          component="img"
-          image={employee.image}
-          alt={employee.firstName}
-          sx={{
-            aspectRatio: '9 / 16',
-          }}
-        />
-        <CardContent sx={{ flexGrow: 1 }}>
-          <h4 className={styles.header}>{employee.firstName}</h4>
-          <p className={`body1 ${styles.paragraph}`}>{employee.profile}</p>
-        </CardContent>
-        <CardActions sx={{ marginInline: 'auto', mb: 2 }}>
-          <Link to="/bookings" onClick={() => handleClick(employee.id)}>
-            <Button
-              size="small"
-              variant="contained"
-            >{`Book with ${employee.firstName}`}</Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
-};
-
-export default function TeamMembers({ employees }) {
+export default function TeamMember() {
   const { handleEmployeeChange } = useFilter();
-  return (
-    <div className={styles.card_container}>
-      <h3 className="text-center">Our Team</h3>
-      <Grid container spacing={4}>
+  let content;
+  const {
+    data: employees,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGetEmployeesProfilesQuery();
+  if (isLoading) {
+    content = Array.from({ length: 3 }, (_, i) => (
+      <Grid item xs={12} sm={6} md={4} sx={{ marginInline: 'auto' }} key={i}>
+        <LoadingSpinner />
+      </Grid>
+    ));
+  }
+  if (isError) {
+    content = <p>Error...</p>;
+  }
+  if (isSuccess) {
+    content = (
+      <>
         {employees.map((employee) => (
           <MemberCard
             key={employee.id}
@@ -56,6 +34,14 @@ export default function TeamMembers({ employees }) {
             handleClick={handleEmployeeChange}
           />
         ))}
+      </>
+    );
+  }
+  return (
+    <div className={styles.card_container}>
+      <h3 className="text-center">Our Team</h3>
+      <Grid container spacing={4}>
+        {content}
       </Grid>
     </div>
   );
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -2,15 +2,13 @@ import Hero from './Hero';
 import Services from './Services';
 import ContactUs from './ContactUs';
 import TeamMembers from './TeamMembers';
-import { useGetEmployeesProfilesQuery } from '@/features/employeeSlice';
 
 export default function Home() {
-  const { data: employees } = useGetEmployeesProfilesQuery();
   return (
     <div className="mb-16">
       <Hero />
       <Services />
-      <TeamMembers employees={employees} />
+      <TeamMembers />
       <ContactUs />
     </div>
   );
